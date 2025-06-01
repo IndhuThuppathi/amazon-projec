a45eff230bec77d13c08cdc2e071f0fc24aaf0c2
indhu <thuppathiindhu8@gmail.com>
2025-06-01 15:42:22 +0530

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
- export const cart=[{
+ export let cart=[{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2,
  },{
@@ -26,3 +26,29 @@
     });
   }
 }
+
+export function removeFromCart(productId) {
+  for (let i = cart.length - 1; i >= 0; i--) {
+    if (cart[i].productId === productId) {
+      cart.splice(i, 1);
+    }
+  }
+}
+
+
+
+
+
+/*
+export function removeFromCart(productId){
+  const newCart=[];
+
+
+  cart.forEach((cartItem)=>{
+      if(cartItem.productId!==productId){
+        newCart.push(cartItem);
+      }
+  });
+  cart=newCart;
+}
+  */
