3fa298179794242afdd07b2b54551d26d0febe9d
indhu <thuppathiindhu8@gmail.com>
2025-08-05 21:44:19 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,59 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderpaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import'../data/backend-practise.js';
-loadProducts(() =>{
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(()=>{
+     
+      resolve('value1');
+     });
+  }),
+  new Promise((resolve)=>{
+      loadCart(()=>{
+        resolve();
+      });
+  })
+
+
+]).then((values)=>{
+  console.log(values);
    renderOrderSummary();
-  renderpaymentSummary();
+     renderpaymentSummary();
 });
+/*
+new Promise((resolve)=>{
+ 
+     loadProducts(()=>{
+     
+      resolve('value1');
+     });
+}).then(()=>{
+  return new Promise((resolve)=>{
+      loadCart(()=>{
+        resolve();
+      });
+  });
+   
+}).then((value)=>{
+    console.log(value);
+     renderOrderSummary();
+     renderpaymentSummary();
+});
+
+
+*/
+
+/*
+
+loadProducts(() =>{
+  loadCart(()=>{
+     renderOrderSummary();
+     renderpaymentSummary();
+  });
+  
+});
+*/
